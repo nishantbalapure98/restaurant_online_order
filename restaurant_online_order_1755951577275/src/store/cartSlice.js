ff793060157ba3049d3d4a73857918a89278ef05
nishantbalapure98 <piyushbalapure98@gmail.com>
2026-01-10 19:04:13 +0530

    Initial commit

---
+++ restaurant_online_order_1755951577275/src/store/cartSlice.js
@@ -0,0 +1,13 @@
+import { createSlice, createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+
+}
+
+const createSlice=createSlice({
+    name:'cart',
+    initialState,
+    reducers:{
+        
+    }
+})
